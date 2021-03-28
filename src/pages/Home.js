6ca41bc75da56d10ae7715f5bb5c3fa6726993d4
amabelle <amabelletr@gmail.com>
2021-03-29 00:59:31 +0300

    Home in progress 1

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,20 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import ReactPlayer from 'react-player';
 
+import Button from '../shared/components/FormElements/Button';
+
+import './Home.css';
+
 const Home = () => {
+  const [isMuted, setIsMuted] = useState(true);
+
   return (
     <React.Fragment>
-      <div style={{ height: '100vh' }}>
+      <div className="player-background">
+        {/* <div className="video-foreground"> */}
         {/* <div style={{ marginTop: '4rem' }}></div> */}
+
         <ReactPlayer
           url="https://www.youtube.com/watch?v=Ach8lgF6-AY&ab_channel=KOANSound"
           className="react-player"
           playing={true}
-          muted={true}
+          muted={isMuted}
           width="100%"
           height="100%"
+          controls={true}
         />
+        {/* </div> */}
+      </div>
+      <div className="content-wrapper">
+        <div className="first">
+          <h1>Hi! I am Amabelle</h1>
+          <h2>isMuted: {isMuted ? 'true' : 'false'}</h2>
+          <Button
+            // style={{ background: 'white' }}
+            onClick={() => setIsMuted(!isMuted)}
+            inverse
+          >
+            {isMuted ? 'Unmute' : 'Mute'}
+          </Button>
+        </div>
+
+        {/* <div className="second">Hoo</div> */}
+        <div className="second">
+          <h3>A front End Web Developer</h3>
+        </div>
+        <div className="second">
+          <h3>Spesielized in ui/ux </h3>
+        </div>
+        <div className="second">
+          <h3>on the search for my next chanlege 🤑</h3>
+          <img
+            src="https://avatars.githubusercontent.com/u/45571546?v=4"
+            style={{
+              clipPath: 'circle(50% at 50% 50%)',
+              maxWidth: '50vw',
+              maxHeight: '50vh',
+            }}
+          ></img>
+        </div>
       </div>
     </React.Fragment>
   );
---
+++ src/shared/components/FormElements/Button.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import './Button.css';
+
+const Button = (props) => {
+  if (props.href) {
+    return (
+      <a
+        className={`button button--${props.size || 'default'} ${
+          props.inverse && 'button--inverse'
+        } ${props.danger && 'button--danger'} ${props.out && 'button--out'}`}
+        href={props.href}
+      >
+        {props.children}
+      </a>
+    );
+  }
+  if (props.to) {
+    return (
+      <Link
+        to={props.to}
+        exact={props.exact}
+        className={`button button--${props.size || 'default'} ${
+          props.inverse && 'button--inverse'
+        } ${props.danger && 'button--danger'} ${
+          props.enter && 'button--enter'
+        } ${props.out && 'button--out'} ${props.stat && 'button--stat'}`}
+      >
+        {props.children}
+      </Link>
+    );
+  }
+
+  return (
+    <button
+      className={`button button--${props.size || 'default'} ${
+        props.inverse && 'button--inverse'
+      } ${props.danger && 'button--danger'} ${props.enter && 'button--enter'} ${
+        props.out && 'button--out'
+      } ${props.big && 'button--big'} ${props.stat && 'button--stat'}`}
+      type={props.type}
+      onClick={props.onClick}
+      disabled={props.disabled}
+    >
+      {props.children}
+    </button>
+  );
+};
+
+export default Button;
