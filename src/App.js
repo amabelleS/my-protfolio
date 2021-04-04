4d8254f0e5fb3e7ccc05626bbdfcab241e64b5c1
amabelle <amabelletr@gmail.com>
2021-04-04 06:07:21 +0300

    pre build / deployment

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import MainNavigation from './shared/components/navigation/MainNavigation';
+// import LoadingSpinner from './shared/components/UIElements/LoadingSpinner';
 
 import Home from './pages/Home';
 import About from './pages/About';
@@ -10,20 +11,26 @@ import Projects from './pages/Projects';
 import Blog from './pages/Blog';
 import Fotter from './shared/components/UIElements/Fotter';
 
-// const Auth = React.lazy(() => import('./user/pages/Auth'));
+// const Home = lazy(() => import('./pages/Home'));
+// const About = React.lazy(() => import('./pages/About'));
+// const Projects = React.lazy(() => import('./pages/Projects'));
+// const Blog = React.lazy(() => import('./pages/Blog'));
+// const Fotter = React.lazy(() =>
+//   import('./shared/components/UIElements/Fotter')
+// );
 
 function App() {
   return (
     <Router>
       <MainNavigation />
+      {/* <Suspense
+        fallback={
+          <div className="center">
+            <LoadingSpinner />
+          </div>
+        }
+      > */}
       <main>
-        {/* <Suspense
-              fallback={
-                <div className="center">
-                  <LoadingSpinner />
-                </div>
-              }
-            > */}
         <Switch>
           <Route exact path="/">
             <Home />
@@ -38,8 +45,8 @@ function App() {
             <Blog />
           </Route>
         </Switch>
-        {/* </Suspense> */}
       </main>
+      {/* </Suspense> */}
       <Fotter />
     </Router>
   );
